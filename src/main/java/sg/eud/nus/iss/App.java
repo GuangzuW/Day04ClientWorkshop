798de4677ac254e0b318721f9ba07ad4eaf99b89
Wang Guangzu <wgz199154@hotmail.com>
2023-01-17 15:41:49 +0800

    add inputstream

--- src/main/java/sg/eud/nus/iss/App.java
+++ src/main/java/sg/eud/nus/iss/App.java
@@ -1,5 +1,18 @@
 package sg.eud.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
  * Hello world!
  */
@@ -11,7 +24,40 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args)throws UnknownHostException, IOException {
+        Socket socket = new Socket("localhost", 7000);
+
+        try(OutputStream os = socket.getOutputStream()){
+            BufferedOutputStream bos= new BufferedOutputStream(os);
+            DataOutputStream dos= new DataOutputStream(bos);
+            Console con= System.console();
+            String readInput="", receivedMessage="";
+            try(InputStream is = socket.getInputStream()) {
+                BufferedInputStream bis= new BufferedInputStream(is);
+                DataInputStream dis=new DataInputStream(bis);
+
+                while (!readInput.equalsIgnoreCase("close")){
+                    readInput=con.readLine("Enter a command: ");
+                    dos.writeUTF(readInput);
+                    dos.flush();
+
+                    receivedMessage=dis.readUTF();
+                    System.out.println(receivedMessage);
+                }
+                dis.close();
+                bis.close();
+                is.close();
+
+            }catch(EOFException e){
+                socket.close();
+            }
+            dos.close();
+            bos.close();
+            os.close();
+            
+
+        }catch(EOFException e){
+            socket.close();
+        }
     }
 }
